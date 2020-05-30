a800446e1ac800f1359066a68ca4d745d23e5d1b
Alexander Törnvall <alextorn.business@gmail.com>
2020-05-30 09:19:16 +0200

    Header component added

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -5,7 +5,6 @@ import "./collection-preview.styles.scss";
 import CollectionItem from "./../collection-item/collection-item.component";
 
 const CollectionPreview = ({ title, items }) => {
-  console.log(items);
   return (
     <div className="collection-preview">
       <h1 className="title">{title.toUpperCase()}</h1>
