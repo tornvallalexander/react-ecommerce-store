b4a694711074c6d91d2e342812ca42ebba4c86ce
Alexander Törnvall <alextorn.business@gmail.com>
2020-06-02 16:16:35 +0200

    Added addItem and removeItem functionality to checkout

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { connect } from "react-redux";
+
+import { clearItemFromCart } from "./../../redux/cart/cart.actions";
 
 // SCSS
 import "./cart-item.styles.scss";
 
-const CartItem = ({ item: { imageUrl, price, name, quantity } }) => {
+const CartItem = ({ item, clearItem }) => {
+  const { imageUrl, price, name, quantity } = item;
   return (
     <div className="cart-item">
       <img src={imageUrl} alt="item" />
@@ -13,8 +17,15 @@ const CartItem = ({ item: { imageUrl, price, name, quantity } }) => {
           {quantity} x ${price}
         </span>
       </div>
+      <span className="cross" onClick={() => clearItem(item)}>
+        &#10005;
+      </span>
     </div>
   );
 };
 
-export default CartItem;
+const mapDispatchToProps = (dispatch) => ({
+  clearItem: (item) => dispatch(clearItemFromCart(item)),
+});
+
+export default connect(null, mapDispatchToProps)(CartItem);
