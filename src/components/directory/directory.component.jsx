c425255a6b6854c3e81d55cdbf60faab374f3921
Alexander Törnvall <alextorn.business@gmail.com>
2020-06-03 14:51:14 +0200

    Added localstorage with redux persist, movied shop items into redux store, new component collection overview

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,26 +1,27 @@
-import React, { Component } from "react";
+import React from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 
-import sections from "../../utils/sections";
+import { selectDirectorySections } from "./../../redux/directory/directory.selectors";
+
 import MenuItem from "../menu-item/menu-item.component";
 
 // SCSS
 import "./directory.styles.scss";
 
-class Directory extends Component {
-  state = {
-    sections,
-  };
-  render() {
-    const { sections } = this.state;
-    return (
-      <div className="directory-menu">
-        {sections.map(({ id, ...sectionProps }) => (
-          // ...sectionProps === imageUrl={imageUrl} linkUrl={linkUrl} title={title} and so on...
-          <MenuItem key={id} {...sectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
+const Directory = ({ sections }) => {
+  return (
+    <div className="directory-menu">
+      {sections.map(({ id, ...sectionProps }) => (
+        // ...sectionProps === imageUrl={imageUrl} linkUrl={linkUrl} title={title} and so on...
+        <MenuItem key={id} {...sectionProps} />
+      ))}
+    </div>
+  );
+};
 
-export default Directory;
+const mapStateToProps = createStructuredSelector({
+  sections: selectDirectorySections,
+});
+
+export default connect(mapStateToProps)(Directory);
